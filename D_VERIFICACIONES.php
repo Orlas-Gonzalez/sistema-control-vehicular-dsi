a5a7e74e96072dbb41ff7ff296348ab94b538740
Orlas <orlando_garcia_glez@hotmail.com>
2020-11-29 19:58:51 -0600

    Inicio de Modificacion de Formularios D

--- D_VERIFICACIONES.php
+++ D_VERIFICACIONES.php
@@ -1,4 +1,14 @@
+<html>
+<head>
+    <link rel=StyleSheet href="S_DFORMULARIOS.css" type="text/css"> 
+    <title>Eliminación de Verificaciones</title>
+</head>
+<body>
+    <div class="Overlay">
+        <img src="Images/CSS/eliminado.png" class="Registro">
+        <a href="C_VERIFICACIONES.php"><button type="button" onclick="" name="SDUButtons" class="Btn">Regresar</button></a><br>
 <?php
+    
     $SKU_Ver = $_GET['SKU_Ver'];
     include("Controlador.php");
     $conn_MYSQL = conectar();
@@ -8,3 +18,6 @@
     print("<b>Número de Registros Eliminados: </b>".$Afectados);    
     cerrar($conn_MYSQL);
 ?>
+    </div>
+</body>
+</html>
