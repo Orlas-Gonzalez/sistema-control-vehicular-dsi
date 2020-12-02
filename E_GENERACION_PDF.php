4ca70a18ae211b28beca92d05ff6e74214d4c5a0
Marcos Orlando García González <75177289+Orlas-Gonzalez@users.noreply.github.com>
2020-12-01 19:25:52 -0600

    Eliminacion de Archivos No Funcionales

--- E_GENERACION_PDF.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require('../DSI/FPDF/fpdf.php'); // Especificación de la ruta para acceder a la libreria
-    $pdf = new FPDF(); // Constructor 
-    $pdf->AddPage("P", "A4", 0); // Agregar Una Pagina - Pramatetros(Oriengtación, Tamaño, Rotación);
-    $pdf->SetFont('Arial','B',16); // Definición de Fuente - Parametro(Familia, Estilo, Tamaño);
-    $pdf->Cell(60,10,'¡Hola, Mundo!', 1); // Escribir Textos - Parametros
-    $pdf->Image("logo.gif", 40, 40, 50, 50);
-    $pdf->Output();
-?>
